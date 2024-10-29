c5f45ac8b372aeaa8d384cee4981d9c693b01081
samtasticc <samantha.henesy@gmail.com>
2024-10-28 22:35:27 -0400

    complete set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import './App.css';
+// Later on, we'll import button components here
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      {
+        // Add all example components:
+        <Button1 buttonText="Example 1" />
+        <Button2 buttonText="Example 2" />
+        <Button3 buttonText="Example 3" />
+        <Button4 buttonText="Example 4" />
+        <Button5 buttonText="Example 5" />
+        <Button6 buttonText="Example 6" />
+      }
+    </div>
+  );
+};
 
-export default App
+export default App;
+
